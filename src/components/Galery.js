a4a02723282a932f2ebd79dcd3b0f09bd193b760
Italo Gusmão <itamarques19@gmail.com>
2021-11-22 19:37:29 -0300

    atualizar HomePage

--- src/components/Galery.js
+++ src/components/Galery.js
@@ -1,48 +1,17 @@
-import example from "../images/logos/Favicon 01.png";
+export default function Galery(props) {
+  return (
+    <>
+      <div className="card p-3 col-4">
+        <img src={props.products[0].image} className="card-img-top" alt={props.products[0].name} />
+        <div className="card-body">
+          <h5 className="card-title">{props.products[0].name}</h5>
 
-export default function Galery(){
-    return(
-        <>
-         <div className="card p-3 col-4">
-            <img src={example} className="card-img-top" alt="example" />
-            <div className="card-body">
-              <h5 className="card-title">Nome da roupa</h5>
-              <p className="card-text">
-                <p className="card-text text-muted">marca</p>
-                <strong>R$ 999,99</strong>
-              </p>
-            </div>
-          </div>
-          <div className="card p-3 col-4">
-            <img src={example} className="card-img-top" alt="example" />
-            <div className="card-body">
-              <h5 className="card-title">Nome da roupa</h5>
-              <p className="card-text">
-                <p className="card-text text-muted">marca</p>
-                <strong>R$ 999,99</strong>
-              </p>
-            </div>
-          </div>
-          <div className="card p-3 col-4">
-            <img src={example} className="card-img-top" alt="example" />
-            <div className="card-body">
-              <h5 className="card-title">Nome da roupa</h5>
-              <p className="card-text">
-                <p className="card-text text-muted">marca</p>
-                <strong>R$ 999,99</strong>
-              </p>
-            </div>
-          </div>
-          <div className="card p-3 col-4">
-            <img src={example} className="card-img-top" alt="example" />
-            <div className="card-body">
-              <h5 className="card-title">Nome da roupa</h5>
-              <p className="card-text">
-                <p className="card-text text-muted">marca</p>
-                <strong>R$ 999,99</strong>
-              </p>
-            </div>
-          </div>
-        </>
-    )
+          <p className="card-text text-muted"><small>{props.products[0].brand}</small></p>
+          <p className="card-text">
+            <strong>{props.products[0].price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</strong>
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
