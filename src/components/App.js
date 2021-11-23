ba376144873cafd3b3103bbcfdbd8dd16acd5bdc
Karen <karen_araujp7@hotmail.com>
2021-11-23 18:06:23 -0300

    addProduct

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import ShoppingCart from "./ShoppingCart";
 import HomePage from "./HomePage";
 import { Routes, Route } from "react-router-dom";
 import ProductDetail from "./ProductDetail"
+import AddProduct from "./AddProduct"
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/productDetail" element={<ProductDetail />} />
         <Route path="/shopping-cart" element={<ShoppingCart />} />
+        <Route path="/add-product" element={<AddProduct />} />
       </Routes>
     </div>
   );
--- src/components/Galery.js
+++ src/components/Galery.js
@@ -2,13 +2,13 @@ export default function Galery(props) {
   return (
     <>
       <div className="card p-3 col-4">
-        <img src={props.products[0].image} className="card-img-top" alt={props.products[0].name} />
+        <img src={props.image} className="card-img-top" alt={props.name} />
         <div className="card-body">
-          <h5 className="card-title">{props.products[0].name}</h5>
+          <h5 className="card-title">{props.name}</h5>
 
-          <p className="card-text text-muted"><small>{props.products[0].brand}</small></p>
+          <p className="card-text text-muted"><small>{props.brand}</small></p>
           <p className="card-text">
-            <strong>{props.products[0].price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</strong>
+            <strong>{props.price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</strong>
           </p>
         </div>
       </div>
